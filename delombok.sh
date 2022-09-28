232f448c2efe28a409b5ea1de15cd117a367f943
Luke Cartey <5377966+lcartey@users.noreply.github.com>
2022-09-28 10:28:27 +0100

    Remove // comments on all lines
    In some cases the diff will put a "+" line after a line-ending comment line, e.g.:
    ```
      // Foo
    +private int field;
    ``` 
    In this case, the field can accidentally get commented out by the removal of the line ending between the two lines. We already have a sed command that handles this case in lines beginning with "+", where we replace `//` with `/*` and add `/**/` to the end of the line, so we can generalize this to all lines to avoid this problem.

--- delombok.sh
+++ delombok.sh
@@ -9,7 +9,7 @@ if git grep -q "^import lombok" '*.java'; then
     DIFF=$(diff -Z -w -b -B --unified=200000 --minimal "$GITHUB_WORKSPACE/$1" "$1")
     if [ $? -ne 0 ]; then
      echo "Ran delombok on $1"
-     echo "$DIFF" | sed 's/^ //g' |sed 's/^-.*$//g' | sed -r 's#\+(.*)//(.*)$#\1/*\2/**/#g' |sed ':a;N;$!ba;s/\n+/ /g' | tail -n +3 | grep -v '\ No newline at end of file' > "$GITHUB_WORKSPACE/$1"
+     echo "$DIFF" | sed 's/^ //g' |sed 's/^-.*$//g' | sed -r 's#(.*)//(.*)$#\1/*\2/**/#g' |sed ':a;N;$!ba;s/\n+/ /g' | tail -n +3 | grep -v '\ No newline at end of file' > "$GITHUB_WORKSPACE/$1"
     fi
   }
   export -f mergeDelombok
